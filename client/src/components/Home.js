930e739bdf4a9f962c19525efc5f12094b463a50
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2022-03-20 13:20:32 -0400

    Home Page fixed to fit 1920X1080

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -19,39 +19,39 @@ const Home = () => {
 
   return (
     <div>
-    <div id='wrapper'>
+    <div id='wrapperHome'>
      
         {isUser ? (
         <button 
             isAuthenticated 
-            className="loginButton"
+            className="loginButtonHome"
             onClick={() => { logout({ returnTo: window.location.origin });}}> Logout 
         </button>
         ) : (
-        <button onClick={loginWithRedirect} className='loginButton'>Login</button>)}
-      <div className='description'>
+        <button onClick={loginWithRedirect} className='loginButtonHome'>Login</button>)}
+      <div className='descriptionHome'>
         <h1>Upgrade your project</h1>
-        <img src={logo} className='mainLogo' alt="logo" />
+        <img src={logo} className='mainLogoHome' alt="logo" />
         <p>Designed to improve your teamwork experience, komanda offers a wide range of possibilities when it comes to project management.</p>
         <p>All within a couple of clicks.</p>
-        <button onClick={()=> { if (isUser) {navigate('/teams-page') } else { loginWithRedirect()}}} className='homeButton'>Try Komanda</button>
+        <button onClick={()=> { if (isUser) {navigate('/teams-page') } else { loginWithRedirect()}}} className='homeButtonHome'>Try Komanda</button>
       </div>
-      <div className = 'sideProfile'>
-        <img src={homeProfile} className='homeProfilePic' alt="You Profile Picture" />
+      <div className = 'sideProfileHome'>
+        <img src={homeProfile} className='homeProfilePicHome' alt="You Profile Picture" />
         {isUser && user.name ? 
-        (<h2 className='loggedUserName'>{user.name.toUpperCase()}</h2>) 
-        : (<h2>You! Yes, you!</h2>)}
+        (<h2 className='loggedUserNameHome'>{user.name.toUpperCase()}</h2>) 
+        : (<h2 className='loggedUserNameHome'>You! Yes, you!</h2>)}
        
-        <button className='profileButton'>Profile</button>
+        <button className='profileButtonHome'>Profile</button>
       </div>
-      <div id='wrapperTutorial'>
-        <div className = 'tutorialVideo'>
+      <div id='wrapperTutorialHome'>
+        <div className = 'tutorialVideoHome'>
           <iframe width="600" height="400" 
             src="https://www.youtube.com/embed/dQw4w9WgXcQ">
           </iframe>
         </div>
-        <div className = 'tutorialText'>
-          <h1 className = 'tutorialTitle'>Tutorial</h1>
+        <div className = 'tutorialTextHome'>
+          <h1 className = 'tutorialTitleHome'>Tutorial</h1>
           <p>Want to dive in and experience everything first hand? Here is a quick tutorial that will introduce to to the basics of this application.</p>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
         </div>
